d47c1dd574e362646851dd3db4b860a1c09e3cfc
ArifCrimson <hakimiarif132@gmail.com>
2025-06-06 14:55:44 +0800

    Category create function done (not tested yet), Installed sweetalert plugins, and not done anything for create page yet for category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CategoryModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
 {
@@ -11,6 +13,27 @@ class CategoryController extends Controller
     }
 
     public function create(){
+        $title = 'Create Category';
         return view('categories.create');
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:150',
+        ], [
+            'name.required'=>'Category name needed!!!!',
+            'name.max'=>'Category name maximum is 150 words exceeded!!!'
+        ]);
+
+        if($validator->fails()){
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+
+        $data = new CategoryModel();
+        $data->name = $request->input('name');
+        $data->save();
+
+        return redirect()->route('category.index')->with('success', 'Category Added!');
+
     }
 }
