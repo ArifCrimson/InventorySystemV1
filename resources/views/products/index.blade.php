94622ae21385fe8be1c738a9575217a44e7f69d7
ArifCrimson <hakimiarif132@gmail.com>
2025-06-17 15:49:09 +0800

    Added controller and model for products, also index but not adjusted yet for products

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('partials.head')
+
+<body>
+    @include('sweetalert::alert')
+    <div class="container-scroller">
+        <!-- partial:../../partials/_navbar.html -->
+        @include('partials.navbar')
+        <!-- partial -->
+        <div class="container-fluid page-body-wrapper">
+            <!-- partial:../../partials/_sidebar.html -->
+            @include('partials.sidebar')
+            <!-- partial -->
+            <div class="main-panel">
+                <div class="content-wrapper">
+                    <div class="row">
+                        <div class="col-lg-8 grid-margin stretch-card">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="d-flex justify-content-between align-items-center mb-3">
+                                        <h4 class="card-title mb-0">List Of Categories</h4>
+                                        <a href="{{ route('category.create') }}"
+                                            class="btn btn-primary btn-rounded btn-fw">Create Category</a>
+                                    </div>
+
+                                    <div class="table-responsive">
+                                        <table class="table table-hover">
+                                            <thead>
+                                                <tr>
+                                                    <th>No.</th>
+                                                    <th>Category Name</th>
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                @foreach ($categoryresult as $result)
+                                                    <tr>
+                                                        <td>
+                                                            {{ $loop->iteration }}
+                                                        </td>
+                                                        <td>
+                                                            {{ $result->name }}
+                                                        </td>
+                                                    </tr>
+                                                @endforeach
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="d-flex justify-content-center mt-4">
+                                        {{ $categoryresult->appends(request()->query())->links('pagination::bootstrap-4') }}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- content-wrapper ends -->
+                <!-- partial:../../partials/_footer.html -->
+                @include('partials.footer')
+                <!-- partial -->
+            </div>
+            <!-- main-panel ends -->
+        </div>
+        <!-- page-body-wrapper ends -->
+    </div>
+    <!-- container-scroller -->
+    <!-- plugins:js -->
+    @include('partials.plugin')
+</body>
+
+</html>
