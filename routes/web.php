f09f133f83a304514d8ca4b50e32f41863683859
ArifCrimson <hakimiarif132@gmail.com>
2025-08-22 15:05:02 +0800

    InventoryController created. make route for index inventory.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InventoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -19,6 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+//category routes
 Route::get('/categoryIndex', [CategoryController::class , 'index'])->name('category.index');
 Route::get('/categoryCreate', [CategoryController::class, 'create'])->name('category.create');
 Route::post('/categoryAdd', [CategoryController::class, 'store'])->name('category.store');
@@ -26,6 +28,7 @@ Route::get('/categoryEdit/{id}', [CategoryController::class, 'edit'])->name('cat
 Route::put('/categoryUpdate/{id}', [CategoryController::class, 'update'])->name('category.update');
 Route::delete('/categoryDelete/{id}', [CategoryController::class, 'delete'])->name('category.delete');
 
+//products routes
 Route::get('/productIndex', [ProductController::class, 'index'])->name('product.index');
 Route::get('/productCreate', [ProductController::class, 'create'])->name('product.create');
 Route::post('/productAdd', [ProductController::class, 'store'])->name('product.store');
@@ -33,4 +36,6 @@ Route::get('/productEdit/{id}',[ProductController::class, 'edit'])->name('produc
 Route::put('/productUpdate/{id}', [ProductController::class, 'update'])->name('product.update');
 Route::delete('/productDelete/{id}', [ProductController::class, 'delete'])->name('product.delete');
 
+//Inventory routes
+Route::get('/inventoryIndex',[InventoryController::class, 'index'])->name('inventory.index');
 require __DIR__.'/auth.php';
